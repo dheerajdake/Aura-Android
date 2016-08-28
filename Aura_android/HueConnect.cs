cd9605aafd89c38b290e09b417a15465a8e48f4b
wangxiaoyunanne <wangxiaoyunanne@gmail.com>
2016-08-27 22:30:08 -0700

    substring to get id and ip

--- Aura_android/HueConnect.cs
+++ Aura_android/HueConnect.cs
@@ -59,10 +59,91 @@ namespace Aura_android
                 //hueItems = JsonConvert.DeserializeObject<List<Hue_Id>>(json);
                 //Extracting fields  //[{"id":"001788fffe1a68f4","internalipaddress":"10.0.0.15"}]
 
-                Console.Write("The Json payload is "); Console.WriteLine(json);
+//hueItems = JsonConvert.DeserializeObject<List<Hue_Id>>(json);
+//Extracting fields  //[{"id":"001788fffe1a68f4","internalipaddress":"10.0.0.15"}]
+//XW's Extracting fields is [{"id":"001788fffe27e619","internalipaddress":"192.168.1.232"},{"id":"001788fffe1a58eb","internalipaddress":"192.168.1.29"}]
 
-                //Console.Write("The count is ");
-                //Console.WriteLine(hueItems);
+                Console.Write("The Json payload is "); Console.WriteLine(json);
+// see how many hubs are there.
+// for XW there are two, for the hackathon there were 6.
+                int countHueHub = 0;
+                for (int i = 0; i < json.Length; i++)
+                {
+                    if (json[i] == '}')
+                    countHueHub++;
+                }
+// the hub count is 2 in XW's home
+                Console.Write("The hub count is "); Console.WriteLine(countHueHub);
+
+
+//print out all hue hubs one by one.
+                string[] hueHubs = new string[countHueHub];
+                int[] start_hue = new int[countHueHub];
+                int[] len_hue = new int[countHueHub];
+                int whichHueHub = 0;
+                for (int i = 0; i < json.Length; i++)
+                {
+                    if (json[i] == '{')
+                    {
+                        start_hue[whichHueHub] = i;
+                        Console.Write("start place "); Console.WriteLine(i);
+                    }
+                    if (json[i] == '}')
+                    {
+                        len_hue[whichHueHub] = i - start_hue[whichHueHub] + 1;
+                        //Console.Write("len "); Console.WriteLine(i);
+                        whichHueHub++;
+                    }
+                }
+                for (int i = 0; i < countHueHub; i++)
+                {
+                    hueHubs[i] = json.Substring(start_hue[i], len_hue[i]);
+                    Console.Write("hub is  "); Console.WriteLine(json.Substring(start_hue[i], len_hue[i]));
+                }
+//end of print all Hue hubs.
+
+// HERE YOU NEED TO CHOOSE WHICH HUB YOU WANT TO USE
+// FOR EXAMPLE YOU WANT TO USE THE FIRST HUB
+                string hub = hueHubs[0];
+
+                Console.Write("hub is  ");Console.WriteLine(hub);
+//for example I use the first hue hub
+
+//	var newhue = new Hue_Id { id = "001788fffe27e619", internalipaddress = "192.168.1.232" };
+//	var newjson = JsonConvert.SerializeObject(newhue);
+
+//    hueItems = JsonConvert.DeserializeObject< Hue_Id >(newjson);
+//	Console.WriteLine("hueItems",newhue.id);
+// using substring get id and ip
+                int[] start_id_ip= new int[2];
+                int[] len_id_ip = new int[2];
+
+                int position = 0;
+
+                for (int i = 0; i <hub.Length ; i++)
+                {
+                    if (hub[i] == '"')
+                    {
+                        position++;
+        //start_hue[whichHueHub] = i;
+        //Console.Write("start place "); Console.WriteLine(i);
+                    }
+                    if (position == 3)
+                    start_id_ip[0] = i + 1;
+                    else if (position == 4)
+                    len_id_ip[0] = i - start_id_ip[0];
+                    else if (position == 7)
+                    start_id_ip[1] = i + 1;
+                    else if (position == 8)
+                    len_id_ip[1] = i - start_id_ip[1];
+    
+                }
+
+                hueItems.id = hub.Substring(start_id_ip[0], len_id_ip[0]);
+                hueItems.internalipaddress = hub.Substring(start_id_ip[1], len_id_ip[1]);
+                Console.WriteLine(len_id_ip[0]);
+                Console.WriteLine(len_id_ip[1]);
+
 
                 mAdapter = new HueAdapter(this, Resource.Layout.HueConnect, hueItems);
 
